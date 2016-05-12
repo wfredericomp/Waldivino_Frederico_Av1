9bd7d3fb43d7f705ca8b68f157c4ef0d2df549d1
waldivino.marques@al.infnet.edu.br <waldivino.marques@al.infnet.edu.br>
2016-05-11 21:45:19 -0300

    Criando DAL do projeto, inserido as classes Cliente e Produto.

---
+++ MODEL/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODEL.DAL
+{
+    public class Cliente
+    {
+        public int ClienteID { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Endereco {get; set;}
+        public string Telefone { get; set; }
+        
+    }
+}
---
+++ MODEL/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODEL
+{
+    class Produto
+    {
+        public int ProdutoID { get; set; }
+        public string Nome { get; set; }
+        public int Estoque { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
